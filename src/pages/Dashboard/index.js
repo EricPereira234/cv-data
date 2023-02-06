4711fadf7eb88706a659b134347935cdd3f499b5
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2023-02-06 17:10:44 -0300

    login

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,25 @@
 import "./dashboard.css";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../services/firebaseConnection";
+import { signOut } from "firebase/auth";
+
 //importando icones
 import {AiOutlinePlusCircle} from "react-icons/ai";
 
 
 export default function Deshboard(){
+
+    async function logout(){
+        await signOut(auth)
+      }
+
     return(
         <>
             <div className="menu-dashboard">
                 <label>Deshboard</label>
                 <Link to={'/'} ><AiOutlinePlusCircle size={25} color={'#fff'} /></Link>
-                <button>Sair</button>
+                <button onClick={logout} >Sair</button>
             </div>
             <div className="card-home" >
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,21 +1,62 @@
 import "./login.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+
+import { auth } from "../../services/firebaseConnection";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+
 
 //importando arquivos
 import Logo from "../../images/logoEric.png";
+
 export default function Login(){
+
+    const navigate = useNavigate();
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+
+    function logar(e) {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, senha).then(() => {
+            //toast.success('bem-vindo de volta!');
+            navigate('/dashboard');
+        }).catch(() => {
+            //toast.error('usuario não logado verifique todos os campos');
+        });
+
+        signInWithEmailAndPassword(auth, email, senha).then(() => {
+            //toast.success('bem-vindo de volta!');
+            navigate('/dashboard');
+        }).catch(() => {
+            //toast.error('usuario não logado verifique todos os campos');
+        });
+
+    }
+
     return(
+        <div className="card" >
         <div className="card-login" >
             <div className="card-logo ">
                 <img src={Logo} alt="logo do sistema" />
             </div>
 
-            <form>
+            <form onSubmit={logar} >
                 <label>Email</label>
-                <input type={'email'} placeholder="exemple@gmail.com" />
+                <input type={'email'} placeholder="exemple@gmail.com" 
+                     value={email}
+                     onChange={(e) => setEmail(e.target.value)}
+                />
                 <label>Senha</label>
-                <input type={'password'} placeholder="*******" />
-                <button>Entrar</button>
+                <input type={'password'} placeholder="*******" 
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                />
+                <button type={'submit'} >Entrar</button>
             </form>
+        </div>
         </div>
     )
 }
