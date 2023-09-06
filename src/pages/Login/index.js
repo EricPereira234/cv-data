3801623403d62ff89e614c67822069aede48fdef
Secretaria <paroquiacocal@gmail.com>
2023-09-06 16:40:04 -0300

    melhorando o login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,7 @@
 import "./login.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 
 
@@ -17,46 +18,44 @@ export default function Login(){
     const navigate = useNavigate();
     const [email, setEmail] = useState();
     const [senha, setSenha] = useState();
+    const [handleLoader, setHandleLoader] = useState('Logar');
 
     function logar(e) {
         e.preventDefault();
+        setHandleLoader('Carregando..')
         signInWithEmailAndPassword(auth, email, senha).then(() => {
-            //toast.success('bem-vindo de volta!');
+            toast.success('bem-vindo de volta!');
+            setHandleLoader('Logar');
             navigate('/dashboard');
         }).catch(() => {
-            //toast.error('usuario não logado verifique todos os campos');
-        });
-
-        signInWithEmailAndPassword(auth, email, senha).then(() => {
-            //toast.success('bem-vindo de volta!');
-            navigate('/dashboard');
-        }).catch(() => {
-            //toast.error('usuario não logado verifique todos os campos');
-        });
+            toast.error('usuario não logado verifique todos os campos');
+            setHandleLoader('Logar');
+        });  
 
     }
 
+
     return(
         <div className="card" >
-        <div className="card-login" >
-            <div className="card-logo ">
-                <img src={Logo} alt="logo do sistema" />
-            </div>
+            <div className="card-login" >
+                <div className="card-logo ">
+                    <img src={Logo} alt="logo do sistema" />
+                </div>
 
-            <form onSubmit={logar} >
-                <label>Email</label>
-                <input type={'email'} placeholder="exemple@gmail.com" 
-                     value={email}
-                     onChange={(e) => setEmail(e.target.value)}
-                />
-                <label>Senha</label>
-                <input type={'password'} placeholder="*******" 
-                    value={senha}
-                    onChange={(e) => setSenha(e.target.value)}
-                />
-                <button type={'submit'} >Entrar</button>
-            </form>
-        </div>
+                <form onSubmit={logar} >
+                    <label>Email</label>
+                    <input type={'email'} placeholder="exemple@gmail.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <label>Senha</label>
+                    <input type={'password'} placeholder="*******"
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                    />
+                    <button type={'submit'} >{handleLoader}</button>
+                </form>
+            </div>
         </div>
     )
 }
